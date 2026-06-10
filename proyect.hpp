ddb8b0090a3037dd62e333c7ca14d4456e3deff6
Ximena <maxilectora@gmail.com>
2026-06-10 12:54:26 -0600

    Mi primer commit: Estructura inicial del proyecto

---
+++ proyect.hpp
@@ -0,0 +1,25 @@
+//Aqui se haran las definiciones de lo usado
+#ifndef PROYECT_HPP
+#define PROYECT_HPP
+#include <iostream>
+using namespace std;
+struct Pregunta{
+    string pregunta;
+    string respuesta1;
+    string respuesta2;
+    string respuesta3;
+    string respuesta4;
+    string correcta;
+    int puntaje;
+};
+struct Materia{
+    string nombre;
+    Pregunta preguntas[5];
+};
+struct Nodo{
+    Pregunta pregunta;
+    Nodo* siguiente;
+    Nodo* anterior;
+};//porque va a ser una lista doblemente enlazada
+
+#endif
